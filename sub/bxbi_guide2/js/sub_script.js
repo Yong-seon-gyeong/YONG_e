42cde9eddbb9e23c243cba8991f87f0601f2ef1e
용선경 <133983127+Yong-seon-gyeong@users.noreply.github.com>
2023-08-01 02:10:34 +0900

    Add files via upload

---
+++ sub/bxbi_guide2/js/sub_script.js
@@ -0,0 +1,19 @@
+$(function(){
+    $(".top>a").click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 500);
+		return false;
+	});
+
+$('.top').hide();
+
+	$(window).scroll(function() {
+		var height = $(window).scrollTop();
+		if (height > 100) {
+			$('.top').fadeIn();
+		} else {
+			$('.top').fadeOut();
+		}
+	});
+});
